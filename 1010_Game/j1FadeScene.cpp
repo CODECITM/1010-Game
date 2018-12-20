e83a04a784470d44343e5a1710fd12256fc52e28
Carles Homs <36209557+ch0m5@users.noreply.github.com>
2018-12-20 11:50:13 +0100

    Updated FadeToBlack to FadeScene
    Improved as well.
    Fade has steps and both "types of fades", so that can be enumerated to make different functionalities.

---
+++ 1010_Game/j1FadeScene.cpp
@@ -0,0 +1,96 @@
+#include "Brofiler/Brofiler.h"
+#include "p2Defs.h"
+#include "p2Log.h"
+#include "j1App.h"
+#include "j1Window.h"
+#include "j1Render.h"
+#include "j1FadeScene.h"
+#include "SDL/include/SDL_render.h"
+#include "SDL/include/SDL_timer.h"
+
+j1FadeScene::j1FadeScene() : j1Module()
+{
+	name.create("fade");
+}
+
+j1FadeScene::~j1FadeScene()
+{}
+
+// Called before render is available
+bool j1FadeScene::Awake(pugi::xml_node& config)
+{
+	delay = config.child("duration").attribute("seconds").as_float();
+
+	return true;
+}
+
+// Called before the first frame, loads assets
+bool j1FadeScene::Start()
+{
+	LOG("Preparing Fade Screen");
+
+	int screenSize = App->win->GetScale();
+	App->win->GetWindowSize((uint&)screen.w, (uint&)screen.h);
+
+	SDL_SetRenderDrawBlendMode(App->render->renderer, SDL_BLENDMODE_BLEND);
+	return true;
+}
+
+// Update: draw background
+bool j1FadeScene::Update()
+{
+	if (step == fade_step::NONE)
+		return true;
+
+	Uint32 now = SDL_GetTicks() - start_time;
+	float normalized = MIN(1.0f, (float)now / (float)total_time);
+
+	switch (step)
+	{
+	case fade_step::FADING:
+	{
+		if (now >= total_time) {
+			step = fade_step::FULLY_FADED;
+		}
+
+	} break;
+	case fade_step::FULLY_FADED:
+	{
+		total_time += total_time;
+		start_time = SDL_GetTicks();
+		step = fade_step::UNFADING;
+	} break;
+	case fade_step::UNFADING:
+	{
+		normalized = 1.0f - normalized;
+
+		if (now >= total_time)
+			step = fade_step::NONE;
+	} break;
+	}
+
+	// Finally render the black square with alpha on the screen
+	SDL_SetRenderDrawColor(App->render->renderer, 0, 0, 0, (Uint8)(normalized * 255.0f));
+	SDL_RenderFillRect(App->render->renderer, &screen);
+
+	return true;
+}
+
+// Fade to black. At mid point change one scene for another
+bool j1FadeScene::FadeToBlack(float time, fade_type type)
+{
+	bool ret = false;
+
+	this->type = type;
+
+	if (step == fade_step::NONE)
+	{
+		step = fade_step::FADING;
+		start_time = SDL_GetTicks();
+		total_time = (Uint32)(time * 0.5f * 1000.0f);
+
+		ret = true;
+	}
+
+	return ret;
+}
---
+++ 1010_Game/j1FadeScene.h
@@ -0,0 +1,70 @@
+#ifndef __j1FADESCENE_H__
+#define __j1FADESCENE_H__	// @Carles
+
+#include "j1Module.h"
+#include "SDL\include\SDL_rect.h"
+
+enum class fade_type
+{
+	NONE = -1,
+	NEXT_LEVEL,
+	RESTART_LEVEL,
+	RESTART_GAME,
+
+	MAX_TYPES
+};
+
+enum class fade_step
+{
+	NONE = -1,
+	FADING,
+	FULLY_FADED,
+	UNFADING
+};
+
+class j1FadeScene : public j1Module	//CHANGE/FIX: FadeToBlack has parameters that do nothing, needs rework
+{
+public:
+	j1FadeScene();
+
+	// Destructor
+	virtual ~j1FadeScene();
+
+	// Called before render is available
+	bool Awake(pugi::xml_node&);
+
+	// Called before the first frame
+	bool Start();
+
+	// Called each loop iteration (graphic)
+	bool Update();
+
+	bool FadeToBlack(float time = 2.0f, fade_type = fade_type::NONE);
+
+public:
+	fade_step GetStep() const {	//IMPROVE: PUT ON CPP
+		return step;
+	}
+	fade_type GetType() const {
+		return type;
+	}
+	fade_type ResetType() {
+		type = fade_type::NONE;
+		return type;
+	}
+	float GetDelay() const {
+		return delay;
+	}
+
+private:
+	float delay;
+	fade_step step = fade_step::NONE;
+	fade_type type;
+
+	Uint32 start_time = 0;
+	Uint32 total_time = 0;
+	SDL_Rect screen = { 0, 0, 0, 0 };
+
+};
+
+#endif //__FACESCENE_H__
